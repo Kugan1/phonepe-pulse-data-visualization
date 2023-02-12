c99a6f4b73e8fe0905c9dc3dfd719a3a37afedbe
N.Kugan <121663752+Kugan1@users.noreply.github.com>
2023-02-12 16:40:53 +0530

    Update phonepe_pulse.py

--- phonepe_pulse.py
+++ phonepe_pulse.py
@@ -3,32 +3,38 @@ import sqlite3
 import streamlit as st
 import plotly.express as px
 import json
+import numpy as np
+#connect sqlite database
+connect = sqlite3.connect('phonepe.db')
+df = pd.read_sql_query("SELECT * from aggregated_name", connect)
  
-connect = sqlite3.connect('pulse.db')
-df = pd.read_sql_query("SELECT * from aggregated_transaction", connect)
-
- 
- # Define a function to create the dashboard
-
-st.title("PhonePe Pulse Data visualization")
-     
-     #  User input
+#Create the dashboard
+st.title("PhonePe Pulse Dashboard")
+    
+# User input
 state = st.selectbox("Select a State", df['aggregate_name'].unique())
 year = st.selectbox("Select a Year", df['year'].unique())
-payment = st.selectbox("Select a payment type", df['name'].unique())
+payment = st.selectbox("Select a payment", df['name'].unique())
      
-     # Filter the data based on user input
+# Filter user input data
 filtered_df = df[(df['aggregate_name'] == state) & (df['year'] == year) & (df['name'] == payment)]
      
-     # Create a bar chart 
-fig = px.bar(filtered_df, x="name", y="amount", color="count",
-            height=500, title="Transaction Type Distribution")
-st.plotly_chart(fig)
-
 india_states = json.load(open("states_india.geojson", "r"))
 state_id_map = {}
 for feature in india_states["features"]:
     feature["id"] = feature["properties"]["state_code"]
     state_id_map[feature["properties"]["st_nm"]] = feature["id"]
 
+df['scale']=np.log10(df['count']) 
 
+fig = px.choropleth(
+    df,
+    locations="state_id",
+    geojson=india_states,
+    color="scale",
+    hover_name="aggregate_name",
+    hover_data=["count"],
+    title="phonepe pulse data",
+     )
+fig.update_geos(fitbounds="locations", visible=False)
+st.plotly_chart(fig, use_container_width=True)
